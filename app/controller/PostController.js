3a3147b2142a29ddef3e74f5e287d74b22e3222d
Syed Nazir Hussain <nazir.hussain@branex.com>
2022-10-18 18:35:53 +0500

    hotfixes

--- app/controller/PostController.js
+++ app/controller/PostController.js
@@ -1,5 +1,5 @@
 const Validator = require('validatorjs')
-const { MongoClient } = require('mongodb')
+const { MongoClient, ObjectId } = require('mongodb')
 const { StatusCodes } = require("http-status-codes")
 
 const url = process.env.DB_URI
@@ -69,12 +69,34 @@ exports.postStore = async (req, res) => {
 
             errors = errors.flatMap(e => e)
             req.flash('validation_errors', errors)
+
+            res.status(StatusCodes.OK).render('post/create')
         } else {
+
             req.app.locals.fields = {}
+
             console.log(req.body)
+            console.log(req.session.user_id)
+
+            if (ObjectId.isValid(req.session.user_id)) {
+                
+                let post = {
+                    title: req.body.title,
+                    body: req.body.body,
+                    user_id: ObjectId(req.session.user_id),
+                    created_at: new Date().toISOString()
+                }
+            
+                let newPost = await client.db("node-mongoose")
+                                            .collection('posts')
+                                            .insertOne(post)
+    
+                console.log(newPost)
+            }
+
+            res.status(StatusCodes.OK).redirect('/posts')
         }
 
-        res.status(StatusCodes.OK).render('post/create')
     } catch (error) {
 
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).render('errors/500', { message: error.message })
